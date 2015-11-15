6c7c478938c4a6aa49b1914ce0463a7c8a7ca9c9
CJSoft <egwcyh@qq.com>
2015-11-15 19:30:28 +0800

    cpplint passed

--- c++/Math/prime/euler_primelist_generating.cpp
+++ c++/Math/prime/euler_primelist_generating.cpp
@@ -1,7 +1,7 @@
 /**
  * @name euler_primelist_generating.cpp
  * @subject Math
- * @author CJSoft(egwcyh@qq.com)
+ * @author CJSoft (egwcyh@qq.com)
  * @Copyright DWTFABG
  * @comment returns a vector containing primes from 2 to upperBound
  */
@@ -17,30 +17,30 @@
 #include <cstring>
 #include <vector>
 
-std::vector<int> genPrime(int upperBound){
-    bool *isPrime=new bool[upperBound+7];
-    int *prime=new int[upperBound+7];
-    memset(isPrime,true,upperBound+7);
-    memset(prime,0,(upperBound+7)*4);
-    int cursor=0;
-    for(int i=2;i<=upperBound;++i){
-        if(isPrime[i])prime[cursor++]=i;
-        for(int j=0;(j<cursor) && (i*prime[j]<=upperBound);++j){
-            isPrime[i*prime[j]]=false;
-            if(i%prime[j]==0)break;
+std::vector < int > genPrime(int upperBound) {
+    bool *isPrime = new bool[upperBound+7];
+    int *prime = new int[upperBound+7];
+    memset(isPrime, true, upperBound+7);
+    memset(prime, 0, (upperBound+7)*4);
+    int cursor = 0;
+    for (int i = 2; i <= upperBound; ++i) {
+        if (isPrime[i])prime[cursor++] = i;
+        for (int j = 0; (j < cursor) && (i*prime[j] <= upperBound); ++j) {
+            isPrime[i*prime[j]] = false;
+            if (i%prime[j] == 0)break;
         }
     }
-    std::vector<int> vec_rtn(prime,prime+cursor);
+    std::vector < int > vec_rtn(prime, prime+cursor);
     return vec_rtn;
 }
 
 /*-------------------------------------------------------*/
 
 #include <iostream>
-int main(){
-    using namespace std;
-    vector<int> p=genPrime(1000);
-    for(int i=0;i<p.size();++i){
-        cout<<p[i]<<endl;
+int main() {
+    using std::out, std::endl;
+    vector < int > p = genPrime(1000);
+    for (int i = 0; i < p.size(); ++i) {
+        cout << p[i] << endl;
     }
 }
