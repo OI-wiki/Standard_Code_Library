9494d07267d9b35862eec07796df69deb0768924
CJSoft <egwcyh@qq.com>
2015-11-15 18:45:30 +0800

    根据cpplint给出的建议做了格式上的修改

--- c++/Math/prime/euler_primelist_generating.cpp
+++ c++/Math/prime/euler_primelist_generating.cpp
@@ -18,29 +18,29 @@
 #include <vector>
 
 std::vector<int> genPrime(int upperBound){
-	bool *isPrime=new bool[upperBound+7];
-	int *prime=new int[upperBound+7];
-	memset(isPrime,true,upperBound+7);
-	memset(prime,0,(upperBound+7)*4);
-	int cursor=0;
-	for(int i=2;i<=upperBound;++i){
-		if(isPrime[i])prime[cursor++]=i;
-		for(int j=0;(j<cursor) && (i*prime[j]<=upperBound);++j){
-			isPrime[i*prime[j]]=false;
-			if(i%prime[j]==0)break;
-		}
-	}
-	std::vector<int> vec_rtn(prime,prime+cursor);
-	return vec_rtn;
-};
+    bool *isPrime=new bool[upperBound+7];
+    int *prime=new int[upperBound+7];
+    memset(isPrime,true,upperBound+7);
+    memset(prime,0,(upperBound+7)*4);
+    int cursor=0;
+    for(int i=2;i<=upperBound;++i){
+        if(isPrime[i])prime[cursor++]=i;
+        for(int j=0;(j<cursor) && (i*prime[j]<=upperBound);++j){
+            isPrime[i*prime[j]]=false;
+            if(i%prime[j]==0)break;
+        }
+    }
+    std::vector<int> vec_rtn(prime,prime+cursor);
+    return vec_rtn;
+}
 
 /*-------------------------------------------------------*/
 
 #include <iostream>
 int main(){
-	using namespace std;
-	vector<int> p=genPrime(1000);
-	for(int i=0;i<p.size();++i){
-		cout<<p[i]<<endl;
-	}
+    using namespace std;
+    vector<int> p=genPrime(1000);
+    for(int i=0;i<p.size();++i){
+        cout<<p[i]<<endl;
+    }
 }
