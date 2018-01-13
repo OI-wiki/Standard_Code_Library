89f324b9a8fc1b9f3495d78ceac4f37a00f4c0f4
Ir1d <sirius.caffrey@gmail.com>
2018-01-13 16:53:04 +0800

    Update

--- Math/prime/getPrime.cpp
+++ Math/prime/getPrime.cpp
@@ -1,7 +1,7 @@
 /**
  * @Author Ir1d(sirius.caffrey@gmail.com)
  * @Copyright SATA
- * @Example http://poj.org/problem?id=3090
+ * @Example http://codevs.cn/problem/2741/
  */
 #include <cstdio>// NOLINT
 #include <cstring>// NOLINT
@@ -32,60 +32,31 @@ const double pi = acos(-1.0);
 const double EPS = 1e-8;
 const int MAXN = 100033;
 
-LL pri[MAXN], cnt = 0;
-LL phi[MAXN];
+int n;
+int pri[MAXN], cnt;
 bool vis[MAXN];
-LL n;
-void get() {
-  phi[1] = 1;
-  f(i, 2, 100000) {
-    if (!vis[i]) {
-      pri[cnt++] = i; phi[i] = i - 1;
-    }
-    f(j, 0, cnt) {
-      if (i * pri[j] > 100000) break;
-      vis[i * pri[j]] = 1;
-      if (i % pri[j] == 0) {
-        phi[i * pri[j]] = phi[i] * pri[j];
-        break;
-      } else phi[i * pri[j]] = phi[i] * (pri[j] - 1);
-    }
-  }
+void init() {
+	g(i, 2, n) {
+		if (!vis[i]) pri[cnt++] = i;
+		f(j, 0, cnt) {
+			if (i * pri[j] > n) break;
+			vis[i * pri[j]] = 1;
+			if (i % pri[j] == 0) break;
+		}
+	}
 }
-LL m. ans;
-LL getPhi(LL x) {
-  LL t = x;
-  for (LL i = 2; i <= m; ++i) {
-    if (x % i == 0) {
-      t = t / i * (i - 1);
-      while (x % i == 0) x /= i
-    }
-  }
-  if (x > 1) t = t / x * (x - 1);
-  return t;
-}
-LL solve(LL n) {
-  LL res = 3;
-  g(i, 2, n) res += 2 * phi[i];
-  return res;
-}
-int t;
 int main() {
 #ifdef LOCAL
   freopen("a.in", "r", stdin);
   freopen("a.out", "w", stdout);
 #endif
 
-  get();
-  scanf("%d", &t);
-  g(i, 1, t) {
-    scanf("%lld", &n);
-    printf("%d %lld %lld\n", i, n, solve(n));
-  }
-  solve(n);
+	scanf("%d", &n);
+	init();
+	f(i, 0, cnt) printf("%d ", pri[i]);
 
 #ifdef LOCAL
-  fclose(stdin); 
+  fclose(stdin);
   fclose(stdout);
 #endif
   return 0;
